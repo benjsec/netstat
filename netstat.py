c802e5e269ccc93a9e6b095f4e9dab54b2605f39
Ben Secretan <benjsec@gmail.com>
2014-05-28 15:40:10 +0100

    Commandline arguments now accepted

--- netstat.py
+++ netstat.py
@@ -3,11 +3,21 @@ import threading
 import socket
 import ipaddress
 import click
+import sys
 
 def is_up(ip):
+    """
+    Test if a device at a specified ip is responding to ping.
+    Returns a subprocess, once complete will have a returncode 0 if device responds.
+    """
     return subprocess.Popen(["ping", "-c 1", "-q", ip], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
 def get_netdevice_list(netlist):
+    """
+    Create threaded processes to ping a list of ip addresses.
+    Once all threads have finished compile a list of which ip
+    addresses responded and return that list.
+    """
     z = []
     for address in netlist:
         p = is_up(str(address))
@@ -22,6 +32,9 @@ def get_netdevice_list(netlist):
     return alive_list
 
 def get_hostname_list(ip_addr_list):
+    """
+    Interrogate a list of ip addresses to get the hostname of the device, return a list of [ip address, hostname]
+    """
     devlist = []
     for ip in ip_addr_list:
         try:
@@ -31,19 +44,32 @@ def get_hostname_list(ip_addr_list):
         devlist.append([ip, hostname])
     return devlist
 
-def get_network(raw_input):
-    return ipaddress.ip_network(raw_input, strict=False)
-
+def get_own_netaddr():
+    """
+    Get the ip address of the computer running the script.
+    """
+    hostname = ''.join((socket.gethostname(), '.local'))
+    netaddr  = ''.join((socket.gethostbyname(hostname), '/24'))
+    return netaddr
 
 @click.command()
-@click.argument('network_address')
+@click.argument('network_address', default = "" )
 def main(network_address):
-    print("Scanning network for live devices.")
-    netlist = get_network(u''.join(network_address))
+    if network_address == "":
+        print("Network address not specified, defaulting to own network.")
+        network_address = get_own_netaddr()
+
+    netlist = ipaddress.ip_network(u''.join(network_address), strict=False)
+    print("Testing addresses %s to %s..." % (ipaddress._find_address_range([x for x in netlist])))
     alivelist = get_netdevice_list(netlist)
     namelist = get_hostname_list(alivelist)
-    for dev in namelist:
-        print(dev)
+    if len(namelist) != 0:
+        print("The following devices are alive")
+        print("%15s  %s" % ("IP Address", "Hostname"))
+        for dev in namelist:
+            print("%15s  %s" % (dev[0], dev[1]))
+    else:
+        print("No devices found.")
 
 if __name__==("__main__"):
     main()
